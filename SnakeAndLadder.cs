09c5f7cad957bf421964e891a62e4dd8b9a0de60
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-04-29 13:47:34 +0530

    UC4_Repeat Till Playing 100th Position

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -13,7 +13,7 @@ namespace SnakeAndLadder
             int position = 0; // initial position
             Console.WriteLine("Welcome to Snake and Ladder game!");
 
-            while (position < 100) // continue playing until position is 100 or more
+            while (position < 100)
             {
                 Console.WriteLine("You are currently at position " + position);
                 Console.WriteLine("Press any key to roll the dice");
@@ -25,35 +25,38 @@ namespace SnakeAndLadder
 
                 Console.WriteLine("You rolled a " + diceRoll);
 
-                // generate a random number between 1 and 3 to determine the option
-                int option = random.Next(1, 4);
-
+                // update position based on dice roll and option
+                int option = random.Next(0, 3); // 0 for no play, 1 for ladder, 2 for snake
                 switch (option)
                 {
-                    case 1: // No Play
-                        Console.WriteLine("No Play - You stay in the same position");
+                    case 0:
+                        Console.WriteLine("No play! You stay at the same position");
                         break;
-                    case 2: // Ladder
-                        int ladder = random.Next(1, 7); // generate a random number for the ladder position
-                        Console.WriteLine("Ladder - You move ahead by " + ladder + " positions");
-                        position += ladder; // update position based on ladder
-                        break;
-                    case 3: // Snake
-                        int snake = random.Next(1, 7); // generate a random number for the snake position
-                        Console.WriteLine("Snake - You move back by " + snake + " positions");
-                        position -= snake; // update position based on snake
-                        if (position < 0) // check if position goes below 0
+                    case 1:
+                        int newPosition = position + diceRoll;
+                        if (newPosition > 100)
                         {
-                            position = 0; // set position to 0 if it goes below 0
+                            Console.WriteLine("Oops! You cannot move ahead from position " + position);
+                            break;
                         }
+                        Console.WriteLine("Congratulations! You found a ladder! Climb up to position " + newPosition);
+                        position = newPosition;
+                        break;
+                    case 2:
+                         newPosition = position - diceRoll;
+                        if (newPosition < 0)
+                        {
+                            Console.WriteLine("Oops! You cannot go below position 0");
+                            position = 0;
+                            break;
+                        }
+                        Console.WriteLine("Oh no! You landed on a snake! Slide down to position " + newPosition);
+                        position = newPosition;
                         break;
                 }
-
-                Console.WriteLine("You are now at position " + position);
             }
 
             Console.WriteLine("Congratulations! You reached the finish line!");
-            Console.WriteLine("Game over!");
             Console.ReadKey();
         }
     }
